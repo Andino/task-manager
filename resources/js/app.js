8f3912b23d5e5df529e83ef6a22ad3c8159a93b1
Andino <diego.andino93@gmail.com>
2023-09-12 17:13:33 -0600

    :sparkles: Vite + Vue configuration added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
 import './bootstrap';
+
+import {createApp} from 'vue'
+
+import App from './App.vue' 
+
+import {createRouter, createWebHistory} from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: () => import('./pages/Homepage.vue')
+        }
+    ],
+})
+
+const app = createApp(App)
+    .use(router)
+    .mount('#app')
---
+++ resources/js/pages/Homepage.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <div class="d-flex justify-content-center align-items-center">
+        <div class="container col-md-6 shadow-lg rounded-2 p-5">
+            <div class="d-flex border-bottom mb-4 align-items-center">
+                <h2>IMP - Sign in</h2>
+            </div>
+            <form class="">
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="username">Username</label>
+                    <input v-model="form.username" type="username" id="username" class="form-control" />
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="password">Password</label>
+                    <input v-model="form.password" type="password" id="password" class="form-control" />
+                </div>
+                <!-- Submit button -->
+                <button v-if="!loader" type="button" class="btn btn-primary btn-block mb-4" @click="sendForm()">
+                    Sign in
+                </button>
+                <button v-else type="button" class="btn btn-primary btn-block mb-4">
+                    <div class="spinner-border" role="status">
+                        <span class="sr-only"></span>
+                    </div>
+                </button>
+
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Hompeage',
+    data () {
+        return {
+            form:{},
+        }
+    },
+    methods: {
+    }
+}
+</script>
