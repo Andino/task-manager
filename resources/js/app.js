955942cf25a544e7c7410bdeeae52fd5839cdc0c
Andino <diego.andino93@gmail.com>
2023-09-12 21:24:16 -0600

    :sparkles: Added the notification configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 
 import {createApp} from 'vue'
 
-import App from './App.vue' 
+import App from './App.vue'
 
 import {createRouter, createWebHistory} from 'vue-router';
 
@@ -14,6 +14,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
 import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons'
+import Notifications from '@kyvg/vue3-notification'
 
 /* add icons to the library */
 library.add([faTrash, faEdit])
@@ -31,4 +32,5 @@ const router = createRouter({
 const app = createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
     .use(router)
+    .use(Notifications)
     .mount('#app')
