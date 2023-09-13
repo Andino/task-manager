c5392d6bb9b569cc0f0313d3512748360444c0a9
Andino <diego.andino93@gmail.com>
2023-09-12 19:21:26 -0600

    :sparkles: added the task api resource and UI integrated with API

--- resources/js/pages/Homepage.vue
+++ resources/js/pages/Homepage.vue
@@ -2,19 +2,18 @@
     <div class="w-full flex justify-center">
         <div class="w-1/2 flex flex-col justify-center items-center rounded-md p-2 m-5 shadow-lg border border-gray-200">
             <h3 class="font-semibold text-2xl m-3">Task Manager</h3>
-            <draggable v-model="list" 
+            <draggable v-model="tasks"
             :animation="200"
             ghost-class="move-event"
             group="users"
             class="w-full max-w-md">
                 <transition-group>
-                    <li v-for="element in list" :key="element.id"
+                    <li v-for="element in tasks" :key="element.id"
                         class="p-4 mb-3 flex justify-between items-center bg-white shadow-md rounded-lg cursor-move border border-gray-200"
                     >
                         <div class="flex items-center w-full">
                             <div class="flex flex-col w-full">
                                 <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{{element.name}}</p>
-                                <p class="ml-2 text-gray-500">Description: {{element.description}}</p>
                                 <p class="ml-2 text-gray-500">Project: {{element.project.name}}</p>
                             </div>
                             <div class="flex justify-end w-1/2">
@@ -37,20 +36,24 @@
             <div class="modal-mask">
                 <div class="modal-wrapper">
                     <div class="modal-container">
-                    
+
                     <h3 class="font-semibold m-3">Create a new task</h3>
 
                     <div class="row m-3">
-                        <label for="first_name" class="block mb-2 text-sm font-medium">Name</label>
-                        <input type="text" id="first_name" class="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required>
+                        <label for="name" class="block mb-2 text-sm font-medium">Name</label>
+                        <input v-model="taskForm.name" type="text" id="name" class="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John Doe" required>
                     </div>
                     <div class="row m-3">
-                        <label for="first_name" class="block mb-2 text-sm font-medium">Description</label>
-                        <input type="text" id="first_name" class="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required>
+                        <label for="project" class="block mb-2 text-sm font-medium">Project</label>
+                        <select v-model="taskForm.project_id" id="project" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                            <option selected>Select a Project</option>
+                            <option v-for="element in projects" :key="element.id" :value="element.id">{{element.name}}</option>
+                        </select>
                     </div>
+
                     <div class="modal-footer flex justify-center">
                         <slot name="header">
-                            <button class="w-1/5 h-10 m-2 rounded-sm bg-green-200" @click="showTaskModal = false">Create</button>
+                            <button class="w-1/5 h-10 m-2 rounded-sm bg-green-200" @click="createTask">Create</button>
                             <button class="w-1/5 h-10 m-2 rounded-sm bg-red-200" @click="showTaskModal = false">Close</button>
                         </slot>
                     </div>
@@ -65,20 +68,20 @@
             <div class="modal-mask">
                 <div class="modal-wrapper">
                     <div class="modal-container">
-                    
+
                     <h3 class="font-semibold m-3">Create a new project</h3>
 
                     <div class="row m-3">
-                        <label for="first_name" class="block mb-2 text-sm font-medium">Name</label>
-                        <input type="text" id="first_name" class="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required>
+                        <label class="block mb-2 text-sm font-medium">Name</label>
+                        <input type="text" v-model="projectForm.name" class="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required>
                     </div>
                     <div class="row m-3">
-                        <label for="first_name" class="block mb-2 text-sm font-medium">Description</label>
-                        <input type="text" id="first_name" class="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required>
+                        <label class="block mb-2 text-sm font-medium">Description</label>
+                        <input type="text" v-model="projectForm.description" class="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required>
                     </div>
                     <div class="modal-footer flex justify-center">
                         <slot name="header">
-                            <button class="w-1/5 h-10 m-2 rounded-sm bg-green-200" @click="showProjectModal = false">Create</button>
+                            <button class="w-1/5 h-10 m-2 rounded-sm bg-green-200" @click="createProject">Create</button>
                             <button class="w-1/5 h-10 m-2 rounded-sm bg-red-200" @click="showProjectModal = false">Close</button>
                         </slot>
                     </div>
@@ -90,6 +93,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import { VueDraggableNext } from 'vue-draggable-next'
 export default {
     name: 'Hompeage',
@@ -98,23 +102,41 @@ export default {
     },
     data () {
         return {
-            form:{},
+            taskForm:{},
+            projectForm:{},
+            tasks:[],
+            projects:[],
             showTaskModal:false,
             showProjectModal:false,
-            list:[
-                {"id": 1, "name": "test", "description": "This is an description", "project": {
-                    "name": "test project"
-                }},
-                {"id": 2, "name": "test2", "description": "This is an description", "project": {
-                    "name": "test project"
-                }},
-                {"id": 3, "name": "test3", "description": "This is an description", "project": {
-                    "name": "test project"
-                }},
-            ]
         }
     },
+    beforeMount() {
+        this.fetchTasks();
+        this.fetchProjects();
+    },
     methods: {
+        async createTask() {
+            let data = this.taskForm;
+            await axios.post('/api/tasks', data);
+            await this.fetchTasks();
+
+        },
+        async createProject() {
+            let data = this.projectForm;
+            await axios.post('/api/projects', data);
+
+
+        },
+        async fetchTasks() {
+            let response = await axios.get('/api/tasks')
+            let {data: {data}} = response
+            this.tasks = data;
+        },
+        async fetchProjects() {
+            let response = await axios.get('/api/projects')
+            let {data} = response
+            this.projects = data;
+        },
     }
 }
 </script>
